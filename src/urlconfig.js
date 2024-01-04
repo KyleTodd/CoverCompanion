f604f2d11446101d0170bc79f8825076a4dc7c89
Kyle Todd <ktodd087@gmail.com>
2024-01-04 19:58:46 +1100

    remove / on url

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,1 +1,1 @@
-export const baseURL = 'https://covercompanion-backendserver-fdrx-main-g6qvsh5ywa-as.a.run.app/';
+export const baseURL = 'https://covercompanion-backendserver-fdrx-main-g6qvsh5ywa-as.a.run.app';
